55ba1b46e5b2bf82a7aaca285be3a6c7c1684ea5
zijian.yi <zijian.yi@yunzhou-tech.com>
2026-09-15 13:53:07 +0800

    增加工作空间,包管理和编译

---
+++ ros_ws/build/example_cpp/CMakeFiles/node_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_01.dir/src/node_01.cpp.o"
+  "node_01"
+  "node_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
